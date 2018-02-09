f21b9955a4a6b5b2e4dbed06d18f18aa010981de
huangaz <huangaz@foxmail.com>
2018-02-09 18:17:41 +0800

    feature: tsdb service

--- service.go
+++ service.go
@@ -1,8 +1,13 @@
 package tsdb
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
+	"os"
+	"sort"
 	"sync"
+	"time"
 
 	"github.com/golang/glog"
 )
@@ -24,7 +29,36 @@ type TsdbConfig struct {
 	DefaultDelta int64
 }
 
-var TSDBConf TsdbConfig
+var (
+	TSDBConf TsdbConfig
+	// 13*2h
+	bucketNum          uint8  = 13
+	shardsNum                 = 10
+	dataDirectory             = DataDirectory_Test
+	keyWriterNum              = 2
+	keyWriterQueueSize uint32 = 100
+	logWiterNum               = 2
+	logWriterQueueSize uint32 = 100
+	bucketSize         uint64 = 2 * 3600
+
+	// 15min
+	allowTimestampBehind uint32 = 15 * 60
+
+	// 1 min
+	allowTimestampAhead uint32 = 60
+
+	// 1 hour
+	cleanInterval = time.Hour
+
+	// 1o min
+	finalizeInterval = 10 * time.Minute
+
+	adjustTimestamp = false
+)
+
+const (
+	SERVICE_NAME = "tsdb"
+)
 
 func init() {
 	TSDBConf.MinTimestampDelta = 3
@@ -48,10 +82,17 @@ type TsdbService struct {
 	sync.RWMutex
 	buckets map[int]*BucketMap
 	ids     []int32
+
+	ctx    context.Context
+	cancel context.CancelFunc
 }
 
 func NewService() *TsdbService {
 	return nil
+}
+
+func createShardPath(shardId int, dataDirectory string) error {
+	return os.MkdirAll(fmt.Sprintf("%s/%d", dataDirectory, shardId), 0755)
 }
 
 func (t *TsdbService) Start() (err error) {
@@ -59,62 +100,70 @@ func (t *TsdbService) Start() (err error) {
 	t.ids = make([]int32, 1)
 	t.ids[0] = 1
 
-	t.buckets = make(map[int]*BucketMap)
+	t.ctx, t.cancel = context.WithCancel(context.Background())
 
-	k := NewKeyListWriter(DataDirectory_Test, 100)
-	b := NewBucketLogWriter(4*3600, DataDirectory_Test, 100, 0)
-
-	for _, shardId := range t.ids {
-		// todo
-		if err := PathCreate(shardId); err != nil {
-			return err
-		}
-		t.buckets[int(shardId)] = NewBucketMap(6, 4*3600, shardId, DataDirectory_Test,
-			k, b, UNOWNED)
+	keyWriters := make([]*KeyListWriter, keyWriterNum)
+	for i := 0; i < len(keyWriters); i++ {
+		keyWriters[i] = NewKeyListWriter(dataDirectory, keyWriterQueueSize)
 	}
 
-	// check
-	go func() {
-		for _, m := range t.buckets {
-			if err := m.SetState(PRE_OWNED); err != nil {
-				glog.Fatal("set state failed")
-			}
-			if err := m.ReadKeyList(); err != nil {
-				glog.Fatal(err)
-			}
-			if err := m.ReadData(); err != nil {
-				glog.Fatal(err)
-			}
-		}
-		for _, m := range t.buckets {
-			more, err := m.ReadBlockFiles()
-			if err != nil {
-				glog.Fatal(err)
-			}
+	bucketLogWriters := make([]*BucketLogWriter, logWiterNum)
+	for i := 0; i < len(bucketLogWriters); i++ {
+		bucketLogWriters[i] = NewBucketLogWriter(bucketSize, dataDirectory,
+			logWriterQueueSize, allowTimestampBehind)
+	}
 
-			for more {
-				more, err = m.ReadBlockFiles()
-				if err != nil {
-					glog.Fatal(err)
-				}
-			}
+	t.buckets = make(map[int]*BucketMap)
+	for i := 0; i < shardsNum; i++ {
+		k := keyWriters[rand.Intn(len(keyWriters))]
+		b := bucketLogWriters[rand.Intn(len(bucketLogWriters))]
+
+		if err := createShardPath(i, dataDirectory); err != nil {
+			return err
 		}
 
-	}()
+		newMap := NewBucketMap(bucketNum, bucketSize, int32(i),
+			dataDirectory, k, b, UNOWNED)
 
+		t.buckets[i] = newMap
+	}
+
+	t.reload()
+
+	go t.cleanWorker()
+	go t.finalizeBucketWorker()
+
+	return nil
+
+}
+
+func (t *TsdbService) Stop() error {
+	t.cancel()
 	return nil
 }
 
 func (t *TsdbService) Put(req *PutRequest) (*PutResponse, error) {
+	t.RLock()
+	defer t.RUnlock()
+
 	res := &PutResponse{}
-	for _, data := range req.Data {
-		m := t.buckets[int(data.Key.ShardId)]
+	for _, dp := range req.Data {
+		m := t.buckets[int(dp.Key.ShardId)]
 		if m == nil {
 			return res, fmt.Errorf("key not exit")
 		}
 
-		newRows, dataPoints, err := m.Put(string(data.Key.Key), &TimeValuePair{Value: data.Value.Value,
-			Timestamp: data.Value.Timestamp}, 0, false)
+		// Adjust 0, late, or early timestamps to now. Disable only for testing.
+		now := time.Now().Unix()
+		if adjustTimestamp {
+			if dp.Value.Timestamp == 0 || dp.Value.Timestamp < now-int64(allowTimestampBehind) ||
+				dp.Value.Timestamp > now+int64(allowTimestampAhead) {
+				dp.Value.Timestamp = now
+			}
+		}
+
+		newRows, dataPoints, err := m.Put(string(dp.Key.Key), &TimeValuePair{Value: dp.Value.Value,
+			Timestamp: dp.Value.Timestamp}, 0, false)
 		if err != nil {
 			return res, err
 		}
@@ -129,44 +178,242 @@ func (t *TsdbService) Put(req *PutRequest) (*PutResponse, error) {
 	return res, nil
 }
 
-func (t *TsdbService) Get(req *GetRequest) (*GetResponse, error) {
-	return nil, nil
-	/*
-		res := &GetResponse{}
-		if lne(req.Key.Key) == 0 {
-			return nil, fmt.Errorf("null key!")
+func (t *TsdbService) Get(req *GetRequest) (res *GetResponse, err error) {
+
+	res = &GetResponse{Data: make([]*DataPoints, len(req.Keys))}
+	for i, key := range req.Keys {
+		dps := &DataPoints{Key: key}
+		if dps.Values, err = t.getOneKey(key, req.Begin, req.End); err != nil {
+			return
+		}
+		res.Data[i] = dps
+	}
+	return
+}
+
+func (t *TsdbService) getOneKey(key *Key, begin, end int64) (res []*TimeValuePair, err error) {
+	t.RLock()
+	t.RUnlock()
+
+	m := t.buckets[int(key.ShardId)]
+	if m == nil {
+		return nil, fmt.Errorf("key not exit")
+	}
+
+	state := m.GetState()
+	switch state {
+	case UNOWNED:
+		return nil, fmt.Errorf("Don't own shard %d", key.ShardId)
+
+	case PRE_OWNED, READING_KEYS, READING_KEYS_DONE,
+		READING_LOGS, PROCESSING_QUEUED_DATA_POINTS:
+		return nil, fmt.Errorf("Shard %d in progress", key.ShardId)
+	default:
+		datas, err := m.Get(string(key.Key), begin, end)
+		if err != nil {
+			return res, err
 		}
 
-		m := t.buckets[int(req.ShardId)]
+		for _, dp := range datas {
+			res = append(res, &TimeValuePair{Value: dp.Value, Timestamp: dp.Timestamp})
+		}
+
+		if state == READING_BLOCK_DATA {
+			glog.V(1).Infof("Shard %d in process", key.ShardId)
+		}
+
+		if begin < m.GetReliableDataStartTime() {
+			glog.V(1).Infof("missing too much data")
+		}
+
+		return res, nil
+
+	}
+
+}
+
+func (t *TsdbService) reload() error {
+	t.RLock()
+	defer t.RUnlock()
+
+	// config adapter not done
+	ids := []int32{1, 3, 5, 7}
+
+	newMap := make(map[int]bool, len(ids))
+	for i := 0; i < len(ids); i++ {
+		id := ids[i]
+		newMap[int(id)] = true
+	}
+
+	var shardToBeAdded []int
+	var shardToBeDropped []int
+
+	for k, v := range t.buckets {
+		state := v.GetState()
+		if _, ok := newMap[k]; ok {
+			if state == UNOWNED {
+				shardToBeAdded = append(shardToBeAdded, k)
+			} else if state == PRE_UNOWNED {
+				v.CancelUnowning()
+			}
+		} else if state == OWNED {
+			shardToBeDropped = append(shardToBeDropped, k)
+		}
+	}
+
+	sort.Ints(shardToBeAdded)
+	sort.Ints(shardToBeDropped)
+
+	go t.addShard(shardToBeAdded)
+	go t.dropShard(shardToBeDropped)
+
+	return nil
+}
+
+func (t *TsdbService) addShard(shards []int) {
+	t.RLock()
+	t.RUnlock()
+
+	for _, shardId := range shards {
+		m := t.buckets[shardId]
 		if m == nil {
-			return nil, fmt.Errorf("key not exit")
+			glog.Infof("%s Invalid shardId :%d", SERVICE_NAME, shardId)
+			continue
 		}
 
-		res.Key = req.Key
-		state := m.GetState()
-		switch state {
-		case UNOWNED:
-			return nil, fmt.Errorf("Don't own shard %d", req.ShardId)
-		case PRE_OWNED, READING_KEYS, READING_KEYS_DONE, READING_LOGS, PROCESSING_QUEUED_DATA_POINTS:
-			return nil, fmt.Errorf("Shard %d in progress", req.ShardId)
-		default:
-			datas, err := m.Get(req.Key, req.Begin, req.End)
+		if m.GetState() >= PRE_OWNED {
+			continue
+		}
+
+		if err := m.SetState(PRE_OWNED); err != nil {
+			glog.Infof("%s %v", SERVICE_NAME, err)
+			continue
+		}
+
+		if err := m.ReadKeyList(); err != nil {
+			glog.Infof("%s %v", SERVICE_NAME, err)
+			continue
+		}
+
+		if err := m.ReadData(); err != nil {
+			glog.Infof("%s %v", SERVICE_NAME, err)
+			continue
+		}
+	}
+
+	go func() {
+		for _, shardId := range shards {
+			m := t.buckets[shardId]
+			if m == nil {
+				glog.Infof("%s Invalid shardId :%d", SERVICE_NAME, shardId)
+				continue
+			}
+
+			if m.GetState() != READING_BLOCK_DATA {
+				continue
+			}
+
+			more, err := m.ReadBlockFiles()
 			if err != nil {
-				return res, err
+				glog.Infof("%s %v", SERVICE_NAME, err)
+				continue
 			}
 
-			for _, dp := range datas {
-				res.Dps = append(res.Dps, &DataPoint{Value: dp.Value, Timestamp: dp.Timestamp})
+			for more {
+				more, err = m.ReadBlockFiles()
+				if err != nil {
+					glog.Infof("%s %v", SERVICE_NAME, err)
+					break
+				}
 			}
-
-			if state == READING_BLOCK_DATA {
-				return res, fmt.Errorf("Shard %d in progress", req.ShardId)
-			} else if req.Begin < m.GetReliableDataStartTime() {
-				return res, fmt.Errorf("missing too much data")
-			}
-
-			return res, nil
 
 		}
-	*/
+	}()
+}
+
+func (t *TsdbService) dropShard(shards []int) error {
+	t.RLock()
+	defer t.RUnlock()
+
+	for _, shardId := range shards {
+		m := t.buckets[shardId]
+		if m == nil {
+			glog.Infof("%s Invalid shardId :%d", SERVICE_NAME, shardId)
+			continue
+		}
+
+		if m.GetState() != OWNED {
+			continue
+		}
+
+		if err := m.SetState(PRE_UNOWNED); err != nil {
+			glog.Infof("%s %v", SERVICE_NAME, err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func (t *TsdbService) cleanWorker() {
+	ticker := time.NewTicker(cleanInterval).C
+
+	for {
+		select {
+		case <-t.ctx.Done():
+			return
+		case <-ticker:
+			t.RLock()
+			defer t.RUnlock()
+
+			for _, v := range t.buckets {
+				state := v.GetState()
+				if state == OWNED {
+					v.CompactKeyList()
+					err := v.DeleteOldBlockFiles()
+					if err != nil {
+						glog.Infof("%s %v", SERVICE_NAME, err)
+						continue
+					}
+				} else if state == PRE_UNOWNED {
+					err := v.SetState(UNOWNED)
+					if err != nil {
+						glog.Infof("%s %v", SERVICE_NAME, err)
+						continue
+					}
+				}
+			}
+		}
+	}
+}
+
+func (t *TsdbService) finalizeBucketWorker() {
+	ticker := time.NewTicker(finalizeInterval).C
+
+	for {
+		select {
+		case <-t.ctx.Done():
+			return
+		case <-ticker:
+			timestamp := time.Now().Unix() - int64(allowTimestampAhead+allowTimestampBehind) -
+				int64(Duration(1, bucketSize))
+			t.finalizeBucket(timestamp)
+		}
+	}
+}
+
+func (t *TsdbService) finalizeBucket(timestamp int64) {
+	go func() {
+		t.RLock()
+		defer t.RUnlock()
+
+		for _, bucket := range t.buckets {
+			bucketToFinalize := bucket.Bucket(timestamp)
+			_, err := bucket.FinalizeBuckets(bucketToFinalize)
+			if err != nil {
+				glog.Infof("%s %v", SERVICE_NAME, err)
+				continue
+			}
+		}
+	}()
 }
--- service_test.go
+++ service_test.go
@@ -1,63 +1,97 @@
 package tsdb
 
 import (
+	"sort"
 	"testing"
+	"time"
 )
 
 func TestServicePutAndGet(t *testing.T) {
-	/*
-		defer FileDelete()
-		tsdb := &TsdbService{}
-		err := tsdb.Start()
-		if err != nil {
-			t.Fatal(err)
-		}
+	defer FileDelete()
+	ts := &TsdbService{}
+	err := ts.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		num := 10
-		putReq := DataGenerator(1, num)
+	num := 10
+	putReq := DataGenerator(1, num)
 
-		time.Sleep(1000 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 
-		putRes, err := tsdb.Put(putReq)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if putRes.N != int32(num) {
-			t.Fatalf("Put failed! Want %d,put %d", num, putRes.N)
-		}
+	putRes, err := ts.Put(putReq)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if putRes.N != int32(num) {
+		t.Fatalf("Put failed! Want %d,put %d", num, putRes.N)
+	}
 
-		time.Sleep(1000 * time.Millisecond)
+	time.Sleep(1000 * time.Millisecond)
 
-		getReq := &GetRequest{
-			Begin: 0,
-			End:   int64(60 * (num + 1)),
-			Keys: []*Key{
-				&Key{
-					ShardId: 1,
-					Key:     putReq.Data[0].Key,
-				},
+	getReq := &GetRequest{
+		Begin: 0,
+		End:   int64(60 * (num + 1)),
+		Keys: []*Key{
+			&Key{
+				ShardId: 1,
+				Key:     putReq.Data[0].Key.Key,
 			},
-		}
-		getRes, err := tsdb.Get(getReq)
-		if err != nil {
-			t.Fatal(err)
-		}
+		},
+	}
+	getRes, err := ts.Get(getReq)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-		fmt.Println(putReq)
-		fmt.Println(getRes)
+	// fmt.Println(putReq)
+	// fmt.Println(getRes)
 
-		if string(getRes.Key) != string(putReq.Datas[0].Key) {
+	if string(getRes.Data[0].Key.Key) != string(putReq.Data[0].Key.Key) {
+		t.Fatal("wrong result")
+	}
+
+	if len(putReq.Data) != len(getRes.Data[0].Values) {
+		t.Fatalf("Length of putReq(%d) and getRes(%d) not equal!", len(putReq.Data), len(getRes.Data[0].Values))
+	}
+
+	for i, dp := range putReq.Data {
+		if dp.Value.Value != getRes.Data[0].Values[i].Value ||
+			dp.Value.Timestamp != getRes.Data[0].Values[i].Timestamp {
 			t.Fatal("wrong result")
 		}
+	}
+}
 
-		if len(putReq.Datas) != len(getRes.Dps) {
-			t.Fatalf("Length of putReq(%d) and getRes(%d) not equal!", len(putReq.Datas), len(getRes.Dps))
-		}
+func TestServiceReload(t *testing.T) {
 
-		for i, data := range putReq.Datas {
-			if data.Value != getRes.Dps[i].Value || data.Timestamp != getRes.Dps[i].Timestamp {
-				t.Fatal("wrong result")
-			}
+	ts := &TsdbService{}
+	err := ts.Start()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	ts.reload()
+	time.Sleep(100 * time.Millisecond)
+
+	var newMap []int
+	for k, v := range ts.buckets {
+		if v.GetState() == OWNED {
+			newMap = append(newMap, k)
 		}
-	*/
+	}
+	sort.Ints(newMap)
+	wantMap := []int{1, 3, 5, 7}
+	// fmt.Println("newMap: ", newMap)
+	// fmt.Println("wantMap: ", wantMap)
+
+	if len(wantMap) != len(newMap) {
+		t.Fatal("wrong result of reload()")
+	}
+	for i := 0; i < len(wantMap); i++ {
+		if wantMap[i] != newMap[i] {
+			t.Fatal("wrong result of reload()")
+		}
+	}
 }
